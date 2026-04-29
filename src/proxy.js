fd222eefa6e65062443673ff4e30a555620f68c5
Eva-004 <foredajannateva@gmail.com>
2026-04-29 20:41:18 +0600

    Proxy file update

--- src/proxy.js
+++ src/proxy.js
@@ -2,13 +2,14 @@ import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
 import { auth } from './lib/auth'
  
-export async function proxy(request) {
+export async function middleware(request) {
     const session = await auth.api.getSession({
     headers: await headers() 
 })
  if(!session){
   return NextResponse.redirect(new URL('/signin', request.url))
  }
+ return NextResponse.next()
   
 }
 
